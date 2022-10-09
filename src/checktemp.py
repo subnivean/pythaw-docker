3d2204ee1f3607e355c6ba0c5703d018b62b58a9
Your Name <your.email@your.com>
2022-10-09 12:10:31 +0000

    Add battery, staleness checks to sensor

--- src/checktemp.py
+++ src/checktemp.py
@@ -2,7 +2,7 @@
 import datetime
 
 # Current directory
-import ambientweather as aw
+from ambientweather import WeatherData
 import mailsend
 from smartplug import SmartPlug
 
@@ -11,20 +11,46 @@ MINTEMP = 36.0
 MAXTEMP = 39.0
 ALERTTEMP = 35.5
 
-SENSORNAME = "temp2f"
+SENSORNUM = 2
+TEMPSENSOR = f"temp{SENSORNUM}f"
+BATTNAME = f"batt{SENSORNUM}"
 
 SPIP = 23  # SmartPlug IP address (in 192.168.1.xx range)
 
 MEANOUTFILE = "/data/meantemps.out"
 
 sp = SmartPlug(SPIP)
+aw = WeatherData(0)
+
+battlevel = float(aw.get_reading(BATTNAME))
+lastreport = aw.get_reading('date')
+lastreportdt = datetime.datetime.fromisoformat(lastreport.split('.')[0])
+
+curtime = datetime.datetime.now()
+curtimeiso = curtime.isoformat().split('.')[0]
+timedeltasec = (curtime - lastreportdt).seconds
+
+# Send 'low battery' message once a day
+if battlevel < 1.0 and curtime.hour == 12 and curtime.minute == 0:
+    mailsend.send(f"Low battery level on sensor #{SENSORNUM}",
+                  f"Current level: {battlevel}")
+
+# Send 'not reporting' message every hour
+if timedeltasec > 3600 and curtime.minute == 0:
+    mailsend.send(f"Sensor #{SENSORNUM} not reporting",
+                  f"Last report: {lastreport}")
+
+#print(f"{battlevel=}")
+#print(f"{lastreport=}")
+#print(f"{lastreportdt=}")
+#print(f"{curtime.hour=}")
+#print(f"{timedeltasec=}")
 
 meanoutfh = open(MEANOUTFILE, "ab", 0)
 
-curtime = datetime.datetime.now().isoformat().split('.')[0]
-tempF = float(aw.get_reading(SENSORNAME))
+tempF = float(aw.get_reading(TEMPSENSOR))
 
-dline = bytes(f"{curtime} {tempF:.2f}\n", "UTF-8")
+dline = bytes(f"{curtimeiso} {tempF:.2f}\n", "UTF-8")
 meanoutfh.write(dline)
 
 if tempF < ALERTTEMP:
